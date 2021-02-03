4efa0edbd64c8591f3d5102365b581503c7b4adf
Calvin Ling <calvinling8@gmail.com>
2021-02-03 14:24:44 -0500

    Initial commit

---
+++ lint-staged.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  '*.ts': [
+    () => 'tsc -p tsconfig.json --noEmit',
+    (filenames) => `eslint ${filenames.join(' ')} --fix --max-warnings 0`,
+    (filenames) =>
+      filenames.map((filename) => `prettier --write '${filename}'`),
+  ],
+};

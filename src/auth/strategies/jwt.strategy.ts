6f6cdb91286bd9b32c39ac58ee0688ed58f2a890
Calvin Ling <calvinling8@gmail.com>
2021-02-05 15:34:14 -0500

    Check whether a string is a JWT before decoding

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,12 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
+import { isJWT } from 'class-validator';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Repository } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 import { AuthService } from '../auth.service';
 import { TokenPurpose } from '../enums/token-purpose.enum';
+import { InvalidTokenException } from '../exceptions/invalid-token.exception';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
 @Injectable()
@@ -25,12 +27,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       ]),
       ignoreExpiration: false,
       secretOrKeyProvider: async (_request, token, done) => {
+        if (!isJWT(token)) {
+          done(new InvalidTokenException());
+          return;
+        }
         try {
           const { sub } = this.jwtService.decode(token) as JwtPayload;
           const user = await this.usersRepository.findOne({
             where: { id: sub },
           });
-          done(null, user.tokenSecret);
+          done(null, user?.tokenSecret);
         } catch (e) {
           done(e);
         }

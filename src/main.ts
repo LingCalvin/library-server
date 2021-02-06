66aa8c0b234a1e09ba3bec1e561a96c8e7af3596
Calvin Ling <calvinling8@gmail.com>
2021-02-05 21:37:58 -0500

    Set up CORS and helmet

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {
 } from 'typeorm-transactional-cls-hooked';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   // Patch TypeORM for easy support of transactions across service layer
@@ -13,8 +14,11 @@ async function bootstrap() {
   patchTypeORMRepositoryWithBaseRepository();
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.CORS_ORIGIN, credentials: true });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  app.use(helmet());
   app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
